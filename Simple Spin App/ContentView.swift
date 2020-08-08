32b1314e55288efa55ac15abb48893fd5ec4a7ff
Sreenivas K <ksreenivascse@gmail.com>
2020-08-08 21:20:02 +0530

    Initial Commit

---
+++ Simple Spin App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simple Spin App
+//
+//  Created by Sreenivas K on 08/08/20.
+//  Copyright © 2020 Sreenivas K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
